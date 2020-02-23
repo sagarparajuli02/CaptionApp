2aae574ded668a0ca2d60681f151b9d2b968ec4e
sagar <offical.sagar@yahoo.com>
2020-02-23 23:29:24 +1100

    Initial commit

---
+++ app/src/main/java/com/caption/facebook/instagram/MainActivity.java
@@ -0,0 +1,75 @@
+package com.caption.facebook.instagram;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        toolbar = getSupportActionBar();
+
+        // load the store fragment by default
+        toolbar.setTitle("Home");
+        loadFragment(new HomeFragment());
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    toolbar.setTitle("Home");
+                    fragment = new HomeFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_popular:
+                    toolbar.setTitle("Popular");
+                    fragment = new PopularFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_new:
+                    toolbar.setTitle("Latest");
+                    fragment = new NewFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_favroites:
+                    toolbar.setTitle("Favorite");
+                    fragment = new FavroiteFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+}
